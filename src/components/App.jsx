f7586eca3e72f33dcd4f6b2c29baa33102ee6f64
Rogue <swguy270@gmail.com>
2025-01-09 23:30:37 -0600

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <Card
+      name="Jack Bauer"
+      imgURL="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      phone="+542 657 8543"
+      email="jack-bauer@ctu.gov"
+    />
+  );
+}
+
+export default App;
